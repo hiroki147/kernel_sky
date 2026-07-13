7d1d923efc5372e3f5a27c773427322b314283a9
hiroki147 <kirakirapummp1220@gmail.com>
2026-07-13 09:06:50 +0900

    Remove dead drm_bridge_disp_param_set (undefined struct members, unused vendor debug code)

--- drivers/gpu/drm/drm_bridge.c
+++ drivers/gpu/drm/drm_bridge.c
@@ -415,13 +415,6 @@ drm_bridge_chain_mode_valid(struct drm_bridge *bridge,
 }
 EXPORT_SYMBOL(drm_bridge_chain_mode_valid);
 
-void drm_bridge_disp_param_set(struct drm_bridge *bridge, int cmd)
-{
-	if (!bridge)
-		return;
-	printk("[bruce] into kernel/msm-kernel\n");
-	drm_bridge_disp_param_set(bridge->next, cmd);
-
 	if (bridge->funcs->disp_param_set)
 		bridge->funcs->disp_param_set(bridge, cmd);
 }
